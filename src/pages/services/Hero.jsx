6c845ff9a9f1c9955dfda728efb9fd541415c239
Fahim <fahimsedeqi961@gmail.com>
2026-04-07 17:55:26 +0430

    The images are updated

--- src/pages/services/Hero.jsx
+++ src/pages/services/Hero.jsx
@@ -1,5 +1,4 @@
 import SubHero from "../about/SubHero";
-import servicesHero from "../../assets/project1.webp";
 
 export default function ServicesHero() {
     return (
@@ -7,7 +6,7 @@ export default function ServicesHero() {
             <SubHero
                 title="Our Services"
                 currentPage="services"
-                bgImage={servicesHero}
+                bgImage={"/projects/project1.webp"}
             />
         </>
     )
